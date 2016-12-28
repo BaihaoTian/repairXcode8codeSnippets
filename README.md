014f1d8d9e7f171066f4620b841442c3adef465c
luandun <luandun@2dfire.com>
2016-12-28 19:33:41 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+###repair XcodeSnippets
+##### BaihaoTian
+####下载下来 放到任意文件夹  直接./repairXcodeSnippets.sh 执行   
+####如果提示 ```permission denied: ./repairXcodeSnippets.sh```
+####执行 ```$ chmod 777 ./repairXcodeSnippets.sh ```
+####  执行完重新打开xcode即可
---
+++ repairXcodeSnippets.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#Bc.wh1te_Le1 
+pid=`ps -ef|grep "Xcode.app/Contents/MacOS/Xcode"|grep -v grep|grep -v PPID|awk '{ print $2}'`
+
+if [[ -z $pid ]] ; then
+  echo "******xcode关闭成功******\n"
+  echo "******开始删除DriveData******"
+
+  	find ~/Library/Developer/Xcode/DerivedData/* -exec rm -fr {} \;
+#  	if [ $? -eq 0 ];then
+		find ~/Library/Caches/com.apple.dt.Xcode -exec rm -fr {} \;
+#			if [ $? -eq 0 ];then
+				echo "******操作成功请重新打开Xcode******"
+#			else
+#				echo "******操作失败请重新执行脚本*******"
+#			fi	
+#	else
+#		echo "******操作失败请重新执行脚本*******"
+#	fi
+else
+  echo "******请关闭xcode再执行此脚本******"
+  echo $pid
+
+fi

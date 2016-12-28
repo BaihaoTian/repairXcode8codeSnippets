611b48d7ce1f23a8bbef01954e0f6960cfd86c23
luandun <luandun@2dfire.com>
2016-12-28 22:41:31 +0800

    fix rm bugs

--- repairXcodeSnippets.sh
+++ repairXcodeSnippets.sh
@@ -6,17 +6,19 @@ if [[ -z $pid ]] ; then
   echo "******xcode关闭成功******\n"
   echo "******开始删除DriveData******"
 
-  	find ~/Library/Developer/Xcode/DerivedData/* -exec rm -fr {} \;
-#  	if [ $? -eq 0 ];then
-		find ~/Library/Caches/com.apple.dt.Xcode -exec rm -fr {} \;
-#			if [ $? -eq 0 ];then
+#  	find ~/Library/Developer/Xcode/DerivedData/* -exec rm -fr {} \;
+  rm -fr ~/Library/Developer/Xcode/DerivedData/*;
+  	if [ $? -eq 0 ];then
+#		find ~/Library/Caches/com.apple.dt.Xcode -exec rm -fr {} \;
+    rm -fr ~/Library/Caches/com.apple.dt.Xcode
+			if [ $? -eq 0 ];then
 				echo "******操作成功请重新打开Xcode******"
-#			else
-#				echo "******操作失败请重新执行脚本*******"
-#			fi	
-#	else
-#		echo "******操作失败请重新执行脚本*******"
-#	fi
+			else
+				echo "******操作失败请重新执行脚本*******"
+			fi
+	else
+		echo "******操作失败请重新执行脚本*******"
+	fi
 else
   echo "******请关闭xcode再执行此脚本******"
   echo $pid
